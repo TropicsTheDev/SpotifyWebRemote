6cbc917fbb99ab2fd5e36efa302d7e07075e9f95
Douglas Townsend <dougymt@gmail.com>
2023-02-22 10:34:49 -0500

    changing to heroku deployments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 export default defineNuxtConfig({
+  nitro: {
+    preset: "cloudflare_pages",
+  },
   modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth", "@pinia/nuxt"],
   runtimeConfig: {
     authOrigin: "",
